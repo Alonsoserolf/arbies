310330932742644f6d360505d42d23d5868f32ac
Alonso Gonzalez <alonsoserolf@gmail.com>
2020-06-12 23:31:18 -0700

    truffle files for contracts

---
+++ contracts/Flashloan.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.6.6;
+
+import "./aave/FlashLoanReceiverBase.sol";
+import "./aave/ILendingPoolAddressesProvider.sol";
+import "./aave/ILendingPool.sol";
+
+contract Flashloan is FlashLoanReceiverBase {
+
+    constructor(address _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
+
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params
+    )
+        external
+        override
+    {
+        require(_amount <= getBalanceInternal(address(this), _reserve), "Invalid balance, was the flashLoan successful?");
+
+        //
+        // Your logic goes here.
+
+        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
+        //
+
+        uint totalDebt = _amount.add(_fee);
+        transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+
+    /**
+        Flash loan 1000000000000000000 wei (1 ether) worth of `_asset`
+     */
+    function flashloan(address _asset) public onlyOwner {
+        // asset address is the coin we will borrow against
+        address receiver = address(this); // Can also be a separate contract
+        uint amount = 1 ether; //1000 * 1e18
+
+        bytes memory params = "";
+        // bytes memory params = abi.encode(address(this), 1234);
+
+        ILendingPool lendingPool = ILendingPool(addressesProvider.getLendingPool());
+        lendingPool.flashLoan(receiver, _asset, amount, params);
+    }
+}
